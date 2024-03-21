4f5ff3b623416188291231acf46ffa5cbb19af82
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-03-21 10:27:02 +0900

    test : test send email code

--- hertz/Projects/Hertz/Tests/Api/UserTest.swift
+++ hertz/Projects/Hertz/Tests/Api/UserTest.swift
@@ -4,8 +4,8 @@ import XCTest
 class MyAppTests: XCTestCase {
     
     func testSignUp() async {
-        let req = SignUpRequest(username: "test", password: "test", passwordCheck: "test")
-        let response = await UserService.shared.signUp(req: req)
+        let req = SignUpRequest(username: "test", password: "test", passwordCheck: "test", code: "")
+        let response = await NetworkService.shared.userService.signUp(req: req)
         printResult(response: response)
         if case .success(let t) = response {
             
@@ -16,20 +16,26 @@ class MyAppTests: XCTestCase {
     
     func testSignIn() async {
         let req = SignInRequest(username: "test", password: "test")
-        let response = await UserService.shared.signIn(req: req)
+        let response = await NetworkService.shared.userService.signIn(req: req)
         printResult(response: response)
     }
     
     func testRefresh() async {
         let refreshToken = ""
-        let response = await UserService.shared.refresh(refreshToken: refreshToken)
+        let response = await NetworkService.shared.userService.refresh(refreshToken: refreshToken)
         printResult(response: response)
         
-        if case .success(let t) = response {
+        if case .success(_) = response {
             
         } else {
             XCTFail("refresh failure")
         }
+    }
+    
+    func testSendEmailCode() async {
+        let req = EmailCodeRequest(to: "hhhello0507@gmail.com")
+        let response = await NetworkService.shared.userService.sendEmailCode(req: req)
+        printResult(response: response)
     }
     
     private func printResult<T>(response: NetworkResult<T>) {
