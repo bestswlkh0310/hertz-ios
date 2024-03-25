5f1711b0b203783c86ca5fbbc94c62997ab555b9
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-03-25 15:24:27 +0900

    feat : add profile info

--- hertz/Projects/Hertz/Sources/Presentation/Main/Profile/View/ProfileView.swift
+++ hertz/Projects/Hertz/Sources/Presentation/Main/Profile/View/ProfileView.swift
@@ -14,20 +14,59 @@ class ProfileView: BaseView {
     
     var username = UILabel()
     
+    var playListTitleContainer = UIView()
+    var playListTitle = UILabel()
+    
     override func setUpStyle() {
         super.setUpStyle()
         
-        image.do { _ in
-//            $0.image
+        image.do {
+            let uiImage = UIImage(named: "DummyProfile")
+            $0.image = uiImage
+        }
+        
+        username.do {
+            $0.text = "dummy"
+            $0.font = .systemFont(ofSize: 16, weight: .semibold)
+        }
+        
+        playListTitle.do {
+            $0.text = "저장된 플레이리스트"
+            $0.font = .systemFont(ofSize: 20, weight: .semibold)
         }
     }
     
     override func configureUI() {
         super.configureUI()
+        addSubviews(image,
+                    username, 
+                    playListTitleContainer)
+        playListTitleContainer.addSubviews(playListTitle)
     }
     
     override func setUpLayout() {
         super.setUpLayout()
+        
+        image.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalTo(safeAreaLayoutGuide).offset(32)
+        }
+        
+        username.snp.makeConstraints {
+            $0.centerY.equalTo(image)
+            $0.leading.equalTo(image.snp.trailing).offset(16)
+        }
+        
+        playListTitleContainer.snp.makeConstraints {
+            $0.top.equalTo(image.snp.bottom).offset(24)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(48)
+        }
+        
+        playListTitle.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20)
+        }
     }
     
 }
